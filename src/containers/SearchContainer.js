ef9d3834ae0a01f4a95ebdef8a1536e54f20ef39
Chase090 <carloyam21@icloud.com>
2021-10-11 02:06:49 -0700

    search functionality buil"

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { connect } from "react-redux";
 class SearchContainer extends React.Component {
 
     state = {
         query: "",
-        
+        tools: []
+    }
+
+    
+
+
+}
+
+const mapStateToProps = (store) => {
+    return{
+        tools: store.tools
     }
 }
+
+
+export default connect(mapStateToProps)(SearchContainer)
